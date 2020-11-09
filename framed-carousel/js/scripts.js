88ae7e0824188d7cd26e8649d170323b14b3e753
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2020-11-08 22:05:18 -0300

    Adiciona modelo framed carousel

---
+++ framed-carousel/js/scripts.js
@@ -0,0 +1,93 @@
+let processingScroll = false;
+let mouseOnScrollable = false;
+let touchOnScrollable = false;
+let startX, startY;
+
+setTimeout(function () {
+    const scrollables = document.getElementsByClassName('scrollable');
+    for (let scrollable of scrollables) {
+        scrollable.addEventListener('mouseenter', () => {
+            mouseOnScrollable = true;
+        });
+    
+        scrollable.addEventListener('mouseleave', () => {
+            mouseOnScrollable = false;
+        });
+
+        scrollable.addEventListener('touchstart', () => {
+            touchOnScrollable = true;
+        });
+    }
+    console.log('Listener para mouse sobre elemento scrollable adicionado');
+
+
+    document.body.addEventListener('wheel', wheelHandler);
+    console.log('Listener para scroll adicionado');
+    
+    document.body.addEventListener('keydown', keydowScrollHandler);
+    console.log('Listener para keydown adicionado');
+
+    
+    
+    document.body.addEventListener('touchstart', touchStartHandler);
+    document.body.addEventListener('touchmove', touchMoveHandler);
+    document.body.addEventListener('touchend', () => {
+        touchOnScrollable = false;
+    });
+    console.log('Listeners para eventos touch adicionados')
+
+}, 1200);
+
+
+function wheelHandler (e) {
+    if (!e.target.classList.contains('scrollable')) {
+        let direction = e.deltaY < 0 ? 'up' : e.deltaY > 0 ? 'down' : null;
+    
+        if (direction && !processingScroll) {
+            processScroll(direction);
+        }
+    }
+}
+
+function keydowScrollHandler (e) {
+    if (!mouseOnScrollable && 
+        document.activeElement.tagName.toLowerCase() === 'body' &&
+        (e.key === 'ArrowUp' || e.key === 'ArrowDown')
+    ) {
+        let direction = e.key === 'ArrowUp' ? 'up' : 'down';
+        processScroll(direction);
+    }
+}
+
+function touchStartHandler (e) {                                    
+    startX = e.touches[0].clientX;                                      
+    startY = e.touches[0].clientY;  
+}
+
+function touchMoveHandler (e) {                                      
+    const posX = e.touches[0].clientX;                                      
+    const posY = e.touches[0].clientY;
+
+    const diffX = posX - startX;
+    const diffY = posY - startY;
+
+    console.log(touchOnScrollable)
+
+    if (!touchOnScrollable && (diffY > 20 || diffY < -20)) {
+        let direction = diffY > 0 ? 'up' : 'down';
+        processScroll(direction);
+    }
+}
+
+
+function processScroll (direction) {
+    window.location.href = direction === 'up' ? topPage : bottomPage;
+    
+    processingScroll = true;
+    setTimeout(function () {
+        setTimeout(function () {
+            processingScroll = false;
+            processingDom.innerHTML = '';
+        }, 1300);
+    }, 1000);
+}
